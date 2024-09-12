8ab79ca5f719b28707b28845d7f95fbf8c1c1597
gabrielneymarteamo <00001120198586sp@al.educacao.sp.gov.br>
2024-09-12 17:44:06 -0300

    alteracao no styles.css e no script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const avanca = document.querySelectorAll ('bnt-proximo')
+const avanca = document.querySelectorAll ('bnt-proximo');
 
 avanca.forEach(button=>{
     button.addEventListener('click',function(){
-        const atual= document.querySelector('.ativo')
-        const proximoPasso = 'Passo-'+this.getAttribute('data-proximo')
+        const atual= document.querySelector('.ativo');
+        const proximoPasso = 'Passo-'+this.getAttribute('data-proximo');
+    atual.classList.remove('ativo');
+    document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
